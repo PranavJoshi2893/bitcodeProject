84bf18b7497a04f3dba92da150d5d54d6a596a7d
pranav <pranavjoshi2893@gmail.com>
2022-12-29 09:46:28 +0530

    slider bar and card animation added

--- src/app/main-page/info-page/course-component/course-component.component.ts
+++ src/app/main-page/info-page/course-component/course-component.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-course-component',
   templateUrl: './course-component.component.html',
   styleUrls: ['./course-component.component.css']
 })
-export class CourseComponentComponent {
+export class CourseComponentComponent implements DoCheck {
+
+  percentage:number=0;
+  cardNumber:number=4;
+  counter=0;
+  devision:number=100/this.cardNumber;
+
+
+  rightButtonColor:string="#4a4a4a";
+  leftButtonColor:string="#4a4a4a";
+
+  cardContainerPosition:number=370;
+
+  
+  onRightClick(){
+    this.percentage+=this.devision;
+    if(this.percentage>=100){
+      this.percentage=100;
+    }
+    else{
+      console.log(this.cardContainerPosition);
+    }
+    
+    
+    
+    
+    this.counter++;
+    if(this.counter>4){
+      this.counter=4;      
+    }
+    else{
+      this.cardContainerPosition-=496;
+    }
+  }
+  
+  onLeftClick(){
+    this.percentage-=this.devision;
+    if(this.percentage<=0){
+      this.percentage=0;
+    }
+    else{
+      console.log(this.cardContainerPosition);
+    }
+    
+    
+    this.counter--;
+    if(this.counter<0){
+      this.counter=0;      
+    }
+    else{
+      this.cardContainerPosition+=496;
+    }
+  }
+  
+
+
+
+  ngDoCheck(): void {
+    if(this.percentage==100){
+      this.rightButtonColor="#1cdac5"
+    }
+    else{
+      this.rightButtonColor="#4a4a4a";
+    }
+    if(this.percentage==0){
+      this.leftButtonColor="#1cdac5";
+    }
+    else{
+      this.leftButtonColor="#4a4a4a";
+    }
+  }
 
 }
