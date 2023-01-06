d6451f3f27acc5534358b791263a298ae1d37ab5
pranav <pranavjoshi2893@gmail.com>
2023-01-07 00:37:32 +0530

    section rev6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InfoPageComponent } from './main-page/info-page/info-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"home",pathMatch:"full"},
+  {path:"home",component:InfoPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
