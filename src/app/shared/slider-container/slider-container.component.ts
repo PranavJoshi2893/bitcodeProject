be9e5e996aa2bfab638e59c5998f732cc25ac2d2
Pranav Joshi <pranavjoshi2893@gmail.com>
2023-02-25 00:12:44 +0530

    branch updated

--- src/app/shared/slider-container/slider-container.component.ts
+++ src/app/shared/slider-container/slider-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input,  Output,  ViewChild } from '@angular/core';
 import { BitcodeService } from 'src/app/bitcode.service';
 
 @Component({
@@ -6,10 +6,11 @@ import { BitcodeService } from 'src/app/bitcode.service';
   templateUrl: './slider-container.component.html',
   styleUrls: ['./slider-container.component.css']
 })
-export class SliderContainerComponent{
+export class SliderContainerComponent implements AfterViewInit{
 
   @Input() id:number=0
-
+  @Output() advProgressPerEmitter = new EventEmitter<number>();
+  @Output() courseProgressPerEmitter = new EventEmitter<number>();
 
   courses:any=[];
   advantages:any=[];
@@ -20,5 +21,38 @@ export class SliderContainerComponent{
     this._bitcodeService.getCardData().subscribe(data=>this.advantages=data[0]);
     this._bitcodeService.getCardData().subscribe(data=>this.courses=data[1]);
   }
+  
+  
+  @ViewChild('advantageCardContainer')
+  advantageCardContainer!:ElementRef;
+  advantageUpdatedPercentage:number=0;
+
+  @ViewChild('courseCardContainer')
+  courseCardContainer!:ElementRef;
+  courseUpdatedPercentage:number=0;
+
+
+  ngAfterViewInit(): void {
+    if(this.id==0){
+      this.advantageCardContainer.nativeElement.addEventListener('scroll', this.advantageProgressBar.bind(this));
+    }
+    else{
+      this.courseCardContainer.nativeElement.addEventListener('scroll', this.courseProgressBar.bind(this));
+    }
+  }
+  advantageProgressBar() {
+    let totalWidth = this.advantageCardContainer.nativeElement.scrollWidth;
+    let scrollPosition = this.advantageCardContainer.nativeElement.scrollLeft;
+    let windowWidth = this.advantageCardContainer.nativeElement.offsetWidth;
+    this.advantageUpdatedPercentage = (scrollPosition / (totalWidth - windowWidth)) * 100;
+    this.advProgressPerEmitter.emit(this.advantageUpdatedPercentage);
+  }
+  courseProgressBar() {
+    let totalWidth = this.courseCardContainer.nativeElement.scrollWidth;
+    let scrollPosition = this.courseCardContainer.nativeElement.scrollLeft;
+    let windowWidth = this.courseCardContainer.nativeElement.offsetWidth;
+    this.courseUpdatedPercentage = (scrollPosition / (totalWidth - windowWidth)) * 100;
+    this.courseProgressPerEmitter.emit(this.courseUpdatedPercentage);
+  }  
 
 }
